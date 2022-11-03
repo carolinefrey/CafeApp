05ea47ef3a4a254080c04934d7927dd267dc11bc
Caroline Frey <cfrey9@gmail.com>
2022-11-03 13:15:27 -0600

    Configure title and image and add menu items.

--- CafeApp/CafeApp/ViewController.swift
+++ CafeApp/CafeApp/ViewController.swift
@@ -10,17 +10,56 @@ import UIKit
 class ViewController: UIViewController {
     
     let cafeTitle = UILabel()
-    let cafeImage = UIImage()
-
+    let cafeImage = UIImageView()
+    let titleStack = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(named: "Background")
+
+        view.addSubview(titleStack)
         
-        view.addSubview(cafeTitle)
-        view.addSubview(cafeImage)
+        configureCafeImage()
+        configureCafeTitle()
+        configureTitleStack()
+        
+        setConstraints()
     }
-
-
+    
+    private func configureCafeImage() {
+        cafeImage.image = UIImage(named: "coffeeTitle")
+        cafeImage.contentMode = .scaleAspectFit
+    }
+    private func configureCafeTitle() {
+        cafeTitle.text = "Hanko Cafe"
+        cafeTitle.textColor = .black
+        cafeTitle.font = UIFont(name: "Inter-Medium", size: 40)
+        cafeTitle.textAlignment = .center
+    }
+    
+    private func configureTitleStack() {
+        titleStack.axis = .horizontal
+        titleStack.alignment = .lastBaseline
+        titleStack.distribution = .fill
+        titleStack.spacing = 15
+    }
+    
+    private func setConstraints() {
+        cafeTitle.translatesAutoresizingMaskIntoConstraints = false
+        cafeImage.translatesAutoresizingMaskIntoConstraints = false
+        titleStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleStack.addArrangedSubview(cafeImage)
+        titleStack.addArrangedSubview(cafeTitle)
+        
+        NSLayoutConstraint.activate([
+            cafeImage.heightAnchor.constraint(equalToConstant: 100),
+            cafeImage.widthAnchor.constraint(equalToConstant: 100),
+            
+            titleStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
 }
 
