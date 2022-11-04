c4a1cffec8b429750ae855b4d8ceeaeafca2bff6
Caroline Frey <cfrey9@gmail.com>
2022-11-03 19:02:44 -0600

    Configure menu table view with item image, name, description and price

---
+++ CafeApp/CafeApp/CustomTableViewCell.swift
@@ -0,0 +1,106 @@
+//
+//  CustomTableViewCell.swift
+//  CafeApp
+//
+//  Created by Caroline Frey on 11/3/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    var backgroundCell = UIView()
+    
+    var itemImageString = String()
+    var itemImage = UIImageView()
+    
+    var itemName = UILabel()
+    var itemDescription = UILabel()
+    var itemPrice = UILabel()
+
+    static let identifier = "CustomTableViewCell"
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(backgroundCell)
+        addSubview(itemImage)
+        addSubview(itemName)
+        addSubview(itemDescription)
+        addSubview(itemPrice)
+        
+        configureBackgroundCell()
+        configureItemImage()
+        configureItemName()
+        configureItemDescription()
+        configureItemPrice()
+        
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(menuItem: MenuItem) {
+        itemImage.image = UIImage(named: "\(menuItem.image)")
+        itemName.text = menuItem.name
+        itemDescription.text = menuItem.description
+        
+        let priceFormat = NumberFormatter()
+        priceFormat.numberStyle = .currency
+
+        itemPrice.text = priceFormat.string(from: NSNumber(value: menuItem.price))
+    }
+    
+    private func configureBackgroundCell() {
+        backgroundCell.backgroundColor = UIColor(named: "Color2")
+    }
+    
+    private func configureItemImage() {
+    }
+    
+    private func configureItemName() {
+        itemName.textColor = .black
+        itemName.font = UIFont(name: "Inter-Medium", size: 20)
+    }
+    
+    private func configureItemDescription() {
+        itemDescription.textColor = .black
+        itemDescription.font = UIFont(name: "Inter-ExtraLight", size: 14)
+    }
+    
+    private func configureItemPrice() {
+        itemPrice.textColor = .black
+        itemPrice.font = UIFont(name: "Inter-Medium", size: 20)
+    }
+    
+    private func setConstraints() {
+        backgroundCell.translatesAutoresizingMaskIntoConstraints = false
+        itemImage.translatesAutoresizingMaskIntoConstraints = false
+        itemName.translatesAutoresizingMaskIntoConstraints = false
+        itemDescription.translatesAutoresizingMaskIntoConstraints = false
+        itemPrice.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            backgroundCell.topAnchor.constraint(equalTo: topAnchor),
+            backgroundCell.bottomAnchor.constraint(equalTo: bottomAnchor),
+            backgroundCell.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundCell.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            itemImage.topAnchor.constraint(equalTo: backgroundCell.topAnchor, constant: 10),
+            itemImage.leadingAnchor.constraint(equalTo: backgroundCell.leadingAnchor, constant: 10),
+            itemImage.heightAnchor.constraint(equalToConstant: 45),
+            itemImage.widthAnchor.constraint(equalToConstant: 45),
+            
+            itemName.topAnchor.constraint(equalTo: backgroundCell.topAnchor, constant: 10),
+            itemName.leadingAnchor.constraint(equalTo: itemImage.trailingAnchor, constant: 10),
+            
+            itemDescription.topAnchor.constraint(equalTo: itemName.bottomAnchor, constant: 5),
+            itemDescription.leadingAnchor.constraint(equalTo: itemImage.trailingAnchor, constant: 10),
+            
+            itemPrice.centerYAnchor.constraint(equalTo: centerYAnchor),
+            itemPrice.trailingAnchor.constraint(equalTo: backgroundCell.trailingAnchor, constant: -10),
+        ])
+    }
+}
--- CafeApp/CafeApp/ViewController.swift
+++ CafeApp/CafeApp/ViewController.swift
@@ -9,20 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let fullMenu = Menu()
+    
     let cafeTitle = UILabel()
     let cafeImage = UIImageView()
     let titleStack = UIStackView()
+    let menu = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(named: "Background")
+        view.backgroundColor = .white
 
         view.addSubview(titleStack)
+        view.addSubview(menu)
         
         configureCafeImage()
         configureCafeTitle()
         configureTitleStack()
+        configureMenu()
         
         setConstraints()
     }
@@ -45,10 +50,21 @@ class ViewController: UIViewController {
         titleStack.spacing = 15
     }
     
+    private func configureMenu() {
+        //menu.delegate = self
+        menu.dataSource = self
+        menu.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
+        
+        menu.clipsToBounds = true
+        menu.layer.cornerRadius = 30
+        menu.rowHeight = 70
+    }
+    
     private func setConstraints() {
         cafeTitle.translatesAutoresizingMaskIntoConstraints = false
         cafeImage.translatesAutoresizingMaskIntoConstraints = false
         titleStack.translatesAutoresizingMaskIntoConstraints = false
+        menu.translatesAutoresizingMaskIntoConstraints = false
         
         titleStack.addArrangedSubview(cafeImage)
         titleStack.addArrangedSubview(cafeTitle)
@@ -59,7 +75,24 @@ class ViewController: UIViewController {
             
             titleStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             titleStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            menu.topAnchor.constraint(equalTo: titleStack.bottomAnchor, constant: 30),
+            menu.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            menu.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            menu.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fullMenu.menuArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier) as! CustomTableViewCell
+        let currentMenuItem = fullMenu.menuArray[indexPath.row]
+        cell.set(menuItem: currentMenuItem)
+        return cell
+    }
+}
